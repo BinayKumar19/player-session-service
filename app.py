15cd5436923c8cc3a65e9c92d2046b417f797ea8
binaykumar19 <binaykumar19@gmail.com>
2019-07-09 23:50:01 -0400

    refactoring

--- app.py
+++ app.py
@@ -4,20 +4,39 @@ from database import CassandraDB
 app = Flask(__name__)
 db = CassandraDB()
 
+
 @app.route('/fetchsession/<player_id>', methods=['GET'])
 def fetch_player_session(player_id):
-    #player_id = request.args.get('player_id')
+    """
+    Returns: Completed events for the given player
+    ===========
+    Description
+    ===========
+    calls database function get_player_events() to fetch completed events from the Database for the given player,
+    Events older than 1 year will be discarded.
+    """
     events = db.get_player_events(player_id)
-    print(events)
-#    return jsonify(events)
-    return events
+    return events.to_json(orient='values')
 
-@app.route('/batch',  methods=['POST'])
+
+@app.route('/batch', methods=['POST'])
 def add_events_batch():
+    """
+    Returns: Status of the insert
+    ===========
+    Description
+    ===========
+    calls database function save_batch() to save a batch into the database,
+    If action performed without error, returns Successful, otherwise Un-Successful.
+    """
     batch = request.get_json()
-    db.save_batch(batch)
-    response = None
+    insert_status = db.save_batch(batch)
+    if insert_status:
+        response = 'Insert Successful'
+    else:
+        response = 'Insert Un-Successful, batch should be in a JSON format'
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run()
--- database.py
+++ database.py
@@ -1,9 +1,32 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from cassandra.cluster import Cluster
 import pandas as pd
 
+"""
+==========
+database
+==========
+This module contains functions to interact with Cassandra.
+==========
+Contents
+----------
+* CassandraDB - Cassandra Database Instance.
+* get_player_events()  - This function will fetch completed events for a player from Cassandra.
+* save_batch()- This function insert a batch of events into Cassandra.
+"""
+
 
 class CassandraDB:
+    """
+    Cassandra Database Instance.
+    ===========
+    Description
+    ===========
+    CassandraDB contains functions to interact with Cassandra database.
+    Details of Cassandra:
+    Keyspace name : sessionkeyspace
+    Tables: start_events and end_events
+    """
 
     def __init__(self):
         self.cluster = Cluster()
@@ -13,26 +36,43 @@ class CassandraDB:
         self.end_events_insert_stmt = self.session.prepare('INSERT INTO end_events(player_id,session_id,ts)'
                                                            ' values(?,?,?)')
 
-        self.events_select_stmt = self.session.prepare('SELECT JSON * FROM end_events WHERE player_id=? and ts > ? LIMIT 20')
+        self.events_select_stmt = self.session.prepare(
+            'SELECT JSON * FROM end_events WHERE player_id=? and ts > ? LIMIT 20')
 
     def get_player_events(self, player):
-        current_time = datetime.now()- timedelta(days=365)
-        # current_time = datetime.strptime(str(now), '%Y-%m-%dT%H:%M:%S')
+        """
+        Returns: Completed events for the given player.
+        ===========
+        Description
+        ===========
+        Fetches completed events from Cassandra Database for the given player,
+        events older than 1 year will be discarded.
+        """
+        current_time = datetime.now() - timedelta(days=365)
+        events = pd.DataFrame(self.session.execute(self.events_select_stmt, (player, current_time)).current_rows)
 
-        rows = pd.DataFrame(self.session.execute(self.events_select_stmt, (player,current_time)).current_rows)
-
-#        return rows.to_json(orient='records')
-        #        {'split','records','index','columns','values','table'}
-        return rows.to_json(orient='values')
+        return events
 
     def save_batch(self, batch):
+        """
+        Returns:
+        ===========
+        Description
+        ===========
+        Saves a batch of events to the Cassandra Database,
+        start events get inserted into start_events
+        end events get inserted into end_events
+        """
+        try:
+            for event in batch:
+                time_stamp = datetime.strptime(event['ts'], '%Y-%m-%dT%H:%M:%S')
 
-        for event in batch:
-            print(event['event'])
-            time_stamp = datetime.strptime(event['ts'], '%Y-%m-%dT%H:%M:%S')
-
-            if event['event'] == 'start':
-                self.session.execute(self.start_events_insert_stmt, (event['country'], event['player_id'],
-                                                                     event['session_id'], time_stamp))
-            else:
-                self.session.execute(self.end_events_insert_stmt, (event['player_id'], event['session_id'], time_stamp))
+                if event['event'] == 'start':
+                    self.session.execute(self.start_events_insert_stmt, (event['country'], event['player_id'],
+                                                                         event['session_id'], time_stamp))
+                else:
+                    self.session.execute(self.end_events_insert_stmt,
+                                         (event['player_id'], event['session_id'], time_stamp))
+        except:
+            return False
+        return True
